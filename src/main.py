a59f8b6819e04d29c0713336012ee4f5c9214819
Harry Le <harryle@multimatic.com>
2025-10-06 17:08:18 -0400

    naming input

--- src/main.py
+++ src/main.py
@@ -2,21 +2,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+def main():
+    fileName = input("Please input file name to generate plot: ") + '.xlsx'
+    genPlot(fileName)
 
-file = "sample_data2.xlsx"
+def genPlot(file):
 
-df = pd.read_excel(file)
+    df = pd.read_excel(file)
 
-mili = df["Milliseconds"]
-value = df["Value"]
-
-
-plt.plot(mili, value, color="orange")
-# plt.scatter(mili, value, color="black")
-
-plt.xlabel("Milisecond")
-plt.ylabel("Value")
-plt.title("Scatter Plot")
+    mili = df["Milliseconds"]
+    value = df["Value"]
 
 
-plt.show()
+    plt.plot(mili, value, color="orange")
+
+    plt.xlabel("Milisecond")
+    plt.ylabel("Value")
+    plt.title("Laser CNC plot")
+
+
+    plt.show()
+
+main()
+
+
+
+
